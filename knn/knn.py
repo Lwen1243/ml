9d46f5ef54502e88ec3b650ca23a62931429d8ad
Lwen1243 <1198807618@qq.com>
2022-03-05 10:57:42 +0800

    knn

--- knn/knn.py
+++ knn/knn.py
@@ -1,42 +1,92 @@
-import numpy as np
-from sklearn.linear_model import LogisticRegression
-import os
-from sklearn.externals import joblib
+import csv
+import random
+import math
+import operator
 
-# 数据预处理
-trainData = np.loadtxt(open('digits_training.csv', 'r'), delimiter=",", skiprows=1)  # 装载数据
-MTrain, NTrain = np.shape(trainData)  # 行列数
-print("训练集：", MTrain, NTrain)
-xTrain = trainData[:, 1:NTrain]
-xTrain_col_avg = np.mean(xTrain, axis=0)  # 对各列求均值
-xTrain = (xTrain - xTrain_col_avg) / 255  # 归一化
-yTrain = trainData[:, 0]
 
-'''================================='''
-# 训练模型
-model = LogisticRegression(solver='lbfgs', multi_class='multinomial', max_iter=500)
-model.fit(xTrain, yTrain)
-print("训练完毕")
+# 加载数据集
+def loadDataset(filename, split, trainingSet=None, testSet=None):
+    if testSet is None:
+        testSet = []
+    if trainingSet is None:
+        trainingSet = []
+    with open(filename, 'r') as csvfile:
+        lines = csv.reader(csvfile)
+        dataset = list(lines)
+        for x in range(len(dataset) - 1):
+            for y in range(4):
+                dataset[x][y] = float(dataset[x][y])
+            if random.random() < split:  # 将数据集随机划分
+                trainingSet.append(dataset[x])
+            else:
+                testSet.append(dataset[x])
 
-'''================================='''
-# 测试模型
-testData = np.loadtxt(open('digits_testing.csv', 'r'), delimiter=",", skiprows=1)
-MTest, NTest = np.shape(testData)
-print("测试集：", MTest, NTest)
-xTest = testData[:, 1:NTest]
-xTest = (xTest - xTrain_col_avg) / 255  # 使用训练数据的列均值进行处理
-yTest = testData[:, 0]
-yPredict = model.predict(xTest)
-errors = np.count_nonzero(yTest - yPredict)  # 返回非零项个数
-print("预测完毕。错误：", errors, "条")
-print("测试数据正确率:", (MTest - errors) / MTest)
 
-'''================================='''
-# 保存模型
+# 计算点之间的距离，多维度的
+def euclideanDistance(instance1, instance2, length):
+    distance = 0
+    for x in range(length):
+        distance += pow((instance1[x] - instance2[x]), 2)
+    return math.sqrt(distance)
 
-# 创建文件目录
-dirs = 'testModel'
-if not os.path.exists(dirs):
-    os.makedirs(dirs)
-joblib.dump(model, dirs + '/model.pkl')
-print("模型已保存")
+
+# 获取k个邻居
+def getNeighbors(trainingSet, testInstance, k):
+    distances = []
+    length = len(testInstance) - 1
+    for x in range(len(trainingSet)):
+        dist = euclideanDistance(testInstance, trainingSet[x], length)
+        distances.append((trainingSet[x], dist))  # 获取到测试点到其他点的距离
+    distances.sort(key=operator.itemgetter(1))  # 对所有的距离进行排序
+    neighbors = []
+    for x in range(k):  # 获取到距离最近的k个点
+        neighbors.append(distances[x][0])
+        return neighbors
+
+
+# 得到这k个邻居的分类中最多的那一类
+def getResponse(neighbors):
+    classVotes = {}
+    for x in range(len(neighbors)):
+        response = neighbors[x][-1]
+        if response in classVotes:
+            classVotes[response] += 1
+        else:
+            classVotes[response] = 1
+    sortedVotes = sorted(classVotes.items(), key=operator.itemgetter(1), reverse=True)
+    return sortedVotes[0][0]  # 获取到票数最多的类别
+
+
+# 计算预测的准确率
+def getAccuracy(testSet, predictions):
+    correct = 0
+    for x in range(len(testSet)):
+        if testSet[x][-1] == predictions[x]:
+            correct += 1
+    return (correct / float(len(testSet))) * 100.0
+
+
+def main():
+    # prepare data
+    trainingSet = []
+    testSet = []
+    split = 0.67
+    loadDataset(r'iris-data.txt', split, trainingSet, testSet)
+    print('Train set: ' + repr(len(trainingSet)))
+    print('Testnet: ' + repr(len(testSet)))
+    # generate predictions
+    predictions = []
+    k = 3
+    for x in range(len(testSet)):
+        # trainingsettrainingSet[x]
+        neighbors = getNeighbors(trainingSet, testSet[x], k)
+        result = getResponse(neighbors)
+        predictions.append(result)
+        print('predicted=' + repr(result) + ', actual=' + repr(testSet[x][-1]))
+    print('predictions: ' + repr(predictions))
+    accuracy = getAccuracy(testSet, predictions)
+    print('Accuracy: ' + repr(accuracy) + '%')
+
+
+if __name__ == '__main__':
+    main()
